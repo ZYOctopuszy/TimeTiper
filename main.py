2c312a4903d37db1919011f3a6a8a3d7eeb4ba18
ZYOctopuszy <2232422807@qq.com>
2025-11-08 20:59:25 +0800

    .
    性能优化

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ from sys import argv
 from classes import *
 
 # 创建日志文件
+log_file_path = os.path.join(os.path.dirname(sys.executable), "TimeTipper.log") if hasattr(sys, "_MEIPASS") else "TimeTipper.log"
+logger.debug(f"日志文件路径: {os.path.abspath(log_file_path)}")
 logger.add(
-    sink=f"{os.path.dirname(__file__)}\\TimeTipper.log",
+    sink=log_file_path,
     rotation="1024 MB",
     retention="2days",
     encoding="utf-8",
 )
-
 
 if __name__ == "__main__":
     app = QApplication(argv)
--- packed.bat
+++ packed.bat
@@ -1,3 +1,4 @@
+@echo off
 echo This file is not used anymore.
 echo Please use packed_by_nuitka.bat instead.
 @REM pyinstaller TimeTipper.spec
